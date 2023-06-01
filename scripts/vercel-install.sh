36ccf68de9a64e49ed94b426da3260828d5a1743
Sheng Lundquist <sheng@delv.tech>
2023-06-01 14:53:27 -0700

    Another try at pyenv

--- scripts/vercel-install.sh
+++ scripts/vercel-install.sh
@@ -1,7 +1,8 @@
-curl https://pyenv.run | bash
-$HOME/.pyenv/bin/pyenv install 3.9
-$HOME/.pyenv/bin/pyenv local 3.9
-$HOME/.pyenv/bin/python -m venv .venv
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+export PATH="$HOME/.pyenv/bin:$PATH"
+pyenv install 3.9
+pyenv local 3.9
+python -m venv .venv
 source .venv/bin/activate
 python3 --version
 which python3
