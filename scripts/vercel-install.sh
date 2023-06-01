7ff5dc9bbaf274e9ffeefe53e37bd3f79a2509c2
Sheng Lundquist <sheng@delv.tech>
2023-06-01 15:08:06 -0700

    More install tests

--- scripts/vercel-install.sh
+++ scripts/vercel-install.sh
@@ -4,7 +4,10 @@ curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer 
 export PATH="$HOME/.pyenv/bin:$PATH"
 pyenv install 3.9
 pyenv local 3.9
-python -m venv .venv
+export PYENV_ROOT="$HOME/.pyenv"
+eval "(pyenv init -)"
+which python3
+python3 -m venv .venv
 source .venv/bin/activate
 python3 --version
 which python3
